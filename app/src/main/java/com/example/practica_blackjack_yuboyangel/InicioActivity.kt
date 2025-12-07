3fd3aa1cf14e96affd3bef8b50db36448c9bc218
Angel <angel.margar@ecuca.jcyl.es>
2025-12-07 16:42:50 +0100

    comentarios musicmanager

--- app/src/main/java/com/example/practica_blackjack_yuboyangel/InicioActivity.kt
+++ app/src/main/java/com/example/practica_blackjack_yuboyangel/InicioActivity.kt
@@ -14,18 +14,15 @@ class InicioActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
 
-        // *** CAMBIO CLAVE 1: Inicializar la música a través del Singleton ***
+
         MusicManager.startMusic(this)
 
         val btnJugar = findViewById<Button>(R.id.btnJugar)
         btnJugar.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-            // !!! IMPORTANTE: NO usamos finish() para que esta Activity
-            // no se destruya inmediatamente y el MediaPlayer del Singleton no sea liberado.
         }
 
-        // *** CAMBIO CLAVE 2: Conectar el botón al Singleton ***
         muteButton = findViewById<ImageButton>(R.id.btnSonido)
 
         // Cargar el icono correcto al inicio, según el estado global
@@ -40,16 +37,11 @@ class InicioActivity : AppCompatActivity() {
 
     override fun onPause() {
         super.onPause()
-        // No pausamos la música aquí, ya que queremos que siga sonando cuando pasemos a MainActivity.
-        // Si quieres pausarla cuando el usuario sale de la app completamente, puedes usar: MusicManager.pauseMusic()
     }
 
     override fun onResume() {
         super.onResume()
-        // Reanudar la música si se pausó por un evento del sistema.
         MusicManager.startMusic(this)
     }
 
-    // Nota: El onDestroy() se omite para evitar que el MusicManager libere la música
-    // mientras la aplicación sigue en memoria.
 }
